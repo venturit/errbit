d85aaebf2e3c512eb4826bfce6951a7569af7a5b
prabode <prabode@gmail.com>
2012-07-19 08:26:02 -0400

    Added mongoid config for errbit

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ puts "Seeding database"
 puts "-------------------------------"
 
 # Create an initial Admin User
-admin_username = "errbit"
-admin_email = "errbit@#{Errbit::Config.host}"
-admin_pass  = 'password'
+admin_username = "prabode"
+admin_email = "prabode@gmail.com"
+admin_pass  = 'm1l3n@2009'
 
 puts "Creating an initial admin user:"
 puts "-- username: #{admin_username}" if Errbit::Config.user_has_username
@@ -13,7 +13,7 @@ puts "-- password: #{admin_pass}"
 puts ""
 puts "Be sure to change these credentials ASAP!"
 user = User.where(:email => admin_email).first || User.new({
-  :name                   => 'Errbit Admin',
+  :name                   => 'Prabode Weebadde',
   :email                  => admin_email,
   :password               => admin_pass,
   :password_confirmation  => admin_pass
